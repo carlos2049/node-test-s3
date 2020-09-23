305f189f6f1e8c0eca27f96ba128e44b7867a90b
carlos <carlos.20499@gmail.com>
2020-09-23 13:17:43 -0300

    added function sendData to UploadController

--- src/controllers/UploadController.js
+++ src/controllers/UploadController.js
@@ -1,4 +1,38 @@
-export const uploadPhoto = (req, res) => {
-  console.log(req.file)
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+dotenv.config()
+
+export const uploadPhoto = async (req, res) => {
+  const data = req.file
+  await envio(sendData)
+
   res.send("subido");
 };
+
+
+const sendData = (data) => {
+
+  const dataName = data.originalname
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+  const s3 = new AWS.S3({
+    accessKeyId,
+    secretAccessKey
+  });
+
+  const parametros = {
+    Bucket: 'gestiona-data'
+  }
+
+  const paramPutObject = {
+    Bucket: 'gestiona-data',
+    Key: dataName,
+    Body: data.buffer
+  }
+  s3.putObject(paramPutObject, (err, data) => {
+    if (err) throw err;
+    console.log(data)
+  })
+
+}
